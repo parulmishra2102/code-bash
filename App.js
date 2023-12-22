42cfab94358bc997bac5d9093a6927ebad6d157d
parulmishra2102 <parulmishra2102@gmail.com>
2023-12-22 12:04:22 +0530

    react element creation and cdn implement

---
+++ App.js
@@ -0,0 +1,3 @@
+const g = React.createElement("div",{className:'parul'},[React.createElement('h2',{id:'rt'},'parul'),React.createElement('h2',{id:'rt1'},'parul1')])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(g)
